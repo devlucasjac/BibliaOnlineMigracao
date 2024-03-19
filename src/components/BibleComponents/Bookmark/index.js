c5104c51cabd33135fabe0adb7e1f5a33dbd6bbf
lucas de souza jacinto <lucasdesouzajacinto@gmail.com>
2024-03-18 22:47:14 -0300

    componente bookmark reestruturado com icones

--- src/components/BibleComponents/Bookmark/index.js
+++ src/components/BibleComponents/Bookmark/index.js
@@ -1,22 +1,29 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CurrentBook from "../../../context/CurrentBook";
 
-import { StyledDiv, StyledTip, StyledMark } from "./styles";
+import { StyledMark } from "./styles";
+
+import BookmarkAddIcon from "@mui/icons-material/BookmarkAdd";
+import BookmarkAddedIcon from "@mui/icons-material/BookmarkAdded";
 
 function Bookmark() {
   const { currentBook } = useContext(CurrentBook);
+  const [savedBible, setSavedBible] = useState(
+    window.localStorage.getItem("Bible")
+  );
+
   return (
     <StyledMark
       style={{ style: "unset" }}
       onClick={() => {
+        setSavedBible(currentBook.bible);
         window.localStorage.setItem("Bible", currentBook.bible);
         window.localStorage.setItem("Book", currentBook.book);
         window.localStorage.setItem("ChapterNum", currentBook.chapterNum);
       }}
     >
-      <StyledDiv></StyledDiv>
-      <StyledTip></StyledTip>
+      {savedBible !== null ? <BookmarkAddedIcon /> : <BookmarkAddIcon />}
     </StyledMark>
   );
 }
