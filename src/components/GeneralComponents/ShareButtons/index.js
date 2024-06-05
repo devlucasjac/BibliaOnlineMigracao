ae7fdd1f9d742e45e85896a2c368ed2552cefee0
lucas <lucasdesouzajacinto@gmail.com>
2024-06-05 17:02:05 -0300

    feat: componente ShareButtons compartilhando imagem gerada no canvas

--- src/components/GeneralComponents/ShareButtons/index.js
+++ src/components/GeneralComponents/ShareButtons/index.js
@@ -1,22 +1,52 @@
+import ShareIcon from '@mui/icons-material/Share';
+import DownloadIcon from '@mui/icons-material/Download';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
+import { IconsContainer } from './style';
+
 function ShareButtons({canva}){
     
-    const exportImage = () => {
-        const canvas = canva.current;
-        const dataUrl = canvas.toDataURL({
-          format: 'png',
-          quality: 1,
-        });
+    const canvas = canva.current;
+    const dataUrl = canvas.toDataURL({
+      format: 'png',
+      quality: 1,
+    });
+
+    const downloadImage = () => {
         const link = document.createElement('a');
         link.href = dataUrl;
         link.download = 'composed-image.png';
         link.click();
     };
 
-    return <>
-        <button onClick={exportImage}>
-            Baixar
-        </button>
-    </>
+    const handleWhats = () => {       
+        const whatsappURL = `https://api.whatsapp.com/send?text=${encodeURIComponent(dataUrl)}`;
+        window.open(whatsappURL, '_blank');
+      };
+
+    const handleShare = () => {                 
+        fetch(dataUrl)
+          .then(res => res.blob())
+          .then(blob => {
+            const file = new File([blob], 'fabric-image.png', { type: 'image/png' });    
+            
+            if (navigator.canShare && navigator.canShare({ files: [file] })) {
+              navigator.share({
+                files: [file],
+                title: 'Imagem Fabric.js',
+                text: 'Olha esta imagem criada com Fabric.js!',
+              }).catch(console.error);
+            } else {
+              alert('Seu navegador não suporta compartilhamento de arquivos.');
+            }
+          });
+    };
+
+    return <IconsContainer>           
+        <DownloadIcon onClick={downloadImage}/>
+        <WhatsAppIcon onClick={handleWhats}/>        
+        <ShareIcon onClick={handleShare}/>
+    </IconsContainer>
 }
 
 export default ShareButtons
---
+++ src/components/GeneralComponents/ShareButtons/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const IconsContainer = styled.div`
+    display:flex;
+    justify-content:space-around;
+    justify-content:flex-end;
+    width:100%;
+    margin-top:10px;
+
+`;
