3aff5813710c6f308df054f75110b099a120b7ac
lucas <lucasdesouzajacinto@gmail.com>
2024-06-17 16:36:01 -0300

    componente Canvas Container redimensiona a imagem de acordo com o tamanho da tela

--- src/components/GeneralComponents/CanvasContainer/index.js
+++ src/components/GeneralComponents/CanvasContainer/index.js
@@ -9,13 +9,10 @@ import cidade from "../../../img/bgImg/cidade.png"
 function CanvasContainer({text,justShare}){
     const canvasRef = useRef(null);
 
-    useEffect(() => {
-      console.log(text)
-      const canvas = new fabric.Canvas(canvasRef.current, {
-        width: 1000,
-        height: 500,
-      });
-  
+    useEffect(() => {      
+      const canvas = new fabric.Canvas(canvasRef.current);
+      canvas.setWidth(window.innerWidth*0.77);
+      canvas.setHeight(window.innerHeight*0.7);
       fabric.Image.fromURL(cidade, (img) => {
         img.set({
           left: 0,
@@ -28,8 +25,7 @@ function CanvasContainer({text,justShare}){
         const textbox = new fabric.Textbox(text, {
           left: canvas.width / 2,
           top: canvas.height / 2,
-          width: 800,
-          maxWidth: 800,
+          width: canvas.width*0.8,          
           originX: 'center',
           originY: 'center',
           fontSize: 30,
